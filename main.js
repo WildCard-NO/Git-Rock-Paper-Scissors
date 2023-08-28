f9d9a2d44e21159551d89e20a3173915cccabac7
WildCard-NO <kalex.boe@gmail.com>
2023-08-28 15:20:13 +0200

    Functions

---
+++ main.js
@@ -0,0 +1,17 @@
+function Gamepad() {
+    //Play the game
+    //Play Five rounds
+    //Console based
+}
+
+function playRound(){
+const playerSelection =
+}
+
+function playerChoice(){
+
+}
+
+function computerChoice(){
+    
+}
