472f18712579bb0549d4072b265e03ff6bc0b62b
WildCard-NO <kalex.boe@gmail.com>
2023-08-29 09:03:16 +0200

    Defines choices

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
-function Gamepad() {
+const choices =["rock","paper","scissors"]
+
+
+function Game() {
+    playRound();
     //Play the game
     //Play Five rounds
     //Console based
 }
 
 function playRound(){
-const playerSelection =
+const playerSelection = playerChoice();
+const computerChoice = computerChoice();
 }
 
 function playerChoice(){
-
+    //Get Input From Player
+    let input = prompt('Type Rock, Paper, or Scissors')
+    //This Forces letters to be LowerCase
+    input = input.toLowerCase();
+    console.log(input)
 }
 
 function computerChoice(){
-    
+
+    //Math.floor rounds up the number so 
+
+    // choices.length limits the choices to 3, if i add +1 in choices it would automatically be put to 4.
+    // easier to use choices.length than 3 in case if changes.
+    return choices[Math.floor(Math.random()*choices.length)]
 }
+
+Game();
