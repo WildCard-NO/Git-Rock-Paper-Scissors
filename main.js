37cb21081668b592617559bac4d6a8ba4393af4b
WildCard-NO <kalex.boe@gmail.com>
2023-09-04 13:54:39 +0200

    unnecessary

--- main.js
+++ main.js
@@ -2,50 +2,17 @@ const choices =["rock","paper","scissors"]
 const winners =[];
 //The game will go for five rounds
 function Game() {
-    for(let i = 1; i <= 5; i++){
-        playRound(i);
-    }
-    //Play the game
-    //Play Five rounds
-    //Console based
+
+        playRound(i);   
 }
 
 function playRound(round){
-const playerSelection = playerChoice();
 const computerSelection = computerChoice();
 //Using "console.log(computerSelection)"
 //We can now see what the computer choose
-console.log(computerSelection);
 const winner = checkWinner(playerSelection, computerSelection)
-console.log(winner)
-winners.push(winner)
-logRound(playerSelection, computerSelection, winner, round)
-}
 
-function playerChoice(){
-    //Get Input From Player
-    let input = prompt("Type Rock, Paper or Scissors")
-    while (input == null) {
-        input = prompt("Type Rock, Paper or Scissors")
-    }
-    input = input.toLowerCase();
-    let check = validateInput(input);
-    while (input == false) {
-        input = prompt(
-            "Type Rock, Paper, or Scissors. The spelling needs to be precise, but capitalization doesn't matter at all"
-            );
-            while (input == null) {
-                input = prompt("Type Rock, Paper or Scissors")
-            }
-            //This Forces letters to be LowerCase
-            input = input.toLowerCase();
-            //If check is valid "in our array (choices (rock, paper, scissors))"
-            //The while loop will become true and break
-            check = validateInput(input);
-        }
-        //Without "return input;"
-        //The computer will only win
-        return input;
+winners.push(winner)
 }
 
 function computerChoice(){
@@ -56,16 +23,6 @@ function computerChoice(){
     // easier to use choices.length than 3 in case if changes.
     return choices[Math.floor(Math.random()*choices.length)]
 }
-
-function validateInput(choice){
-    if (choices.includes(choice)){
-        return true;
-    }   else{
-        return false;
-    }
-}
-
-
 
 function checkWinner(choiceP, choiceC){
     if(choiceP === choiceC){
@@ -79,25 +36,12 @@ function checkWinner(choiceP, choiceC){
     }   else{
         return "Computer";
     }
-
 }
 
 function logWins() {
     let playerWins = winners.filter((item) => item == "Player").length;
     let computerWins = winners.filter((item) => item == "Computer").length;
     let ties = winners.filter((item) => item == "Tie").length;
-    console.log("Results:");
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", computerWins);
-    console.log("Ties:", ties);
-}
-
-function logRound(round, playerChoice, computerChoice, winner){
-    console.log("Round:", round)
-    console.log("Player Chose:", playerChoice)
-    console.log("Computer Chose:", computerChoice)
-    console.log("Won The Round", winner)
-    console.log("-----------------------------------");
 }
 
 Game();
